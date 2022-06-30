ddd974706a9dddcd0731f20a6082c496592bca23
owain-williams <owain@andersdigital.com>
2022-06-30 11:29:42 +0100

    added "generate password" functionality and copies generated password to clipboard

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+from pyperclip import copy
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    pass
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+    password = "".join(password_list)
+
+    print(f"Your password is: {password}")
+    password_input.delete("0.1", END)
+    password_input.insert("0.1", password)
+    copy(password)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = website_input.get("1.0", "end-1c")
@@ -13,14 +34,16 @@ def save_password():
         messagebox.showinfo(title="Error", message="Please don't leave an empty fields")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\n"
-                                                      f"Email: {username}\n"
-                                                      f"Password: {password}\n"
-                                                      f"Is it ok to save?")
+                                                              f"Email: {username}\n"
+                                                              f"Password: {password}\n"
+                                                              f"Is it ok to save?")
         if is_ok:
             with open("passwords.txt", "a") as f:
                 f.writelines(f"{website} | {username} | {password}\n")
             website_input.delete("0.1", END)
             password_input.delete("0.1", END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 # Window setup
@@ -60,7 +83,7 @@ password_label.grid(row=3, column=0)
 password_input = Text(height=1, width=21, bg="white")
 password_input.grid(row=3, column=1)
 # button
-password_button = Button(text="Generate Password",padx=0, command=generate_password)
+password_button = Button(text="Generate Password", padx=0, command=generate_password)
 password_button.grid(row=3, column=2)
 
 # "Add" button - Row 4
